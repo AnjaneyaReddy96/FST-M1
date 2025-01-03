91d484bf6648ccbf21cc692f53d2cd18f59ba445
AnjaneyaReddy96 <ajreddyedula@gmail.com>
2025-01-03 15:32:14 +0530

    Add files via upload

---
+++ Python/Activities/Activity8.py
@@ -0,0 +1,12 @@
+
+numberList = [5, 6, 7, 8, 20]
+print("Given list is ", numberList)
+
+firstElement = numberList[0]
+
+lastElement = numberList[-1]
+
+if (firstElement == lastElement):
+    print(True)
+else:
+    print(False)
