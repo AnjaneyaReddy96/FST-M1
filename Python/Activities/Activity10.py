fdb0988240eaf3296b8b0a17bed02ac65ca996c4
AnjaneyaReddy96 <ajreddyedula@gmail.com>
2025-01-03 15:47:03 +0530

    Add files via upload

---
+++ Python/Activities/Activity10.py
@@ -0,0 +1,8 @@
+
+num_tuple = (22, 13, 43, 46, 55,8)
+print("Given list is ", num_tuple)
+ 
+print("Elements that are divisible by 5:")
+for num in num_tuple:
+    if (num % 2 == 0):
+        print(num)
