e9d026a2e8b744d56fbe1976b3f3beb8c2fe9807
AnjaneyaReddy96 <ajreddyedula@gmail.com>
2025-02-09 19:09:48 +0530

    Add files via upload

---
+++ TestNG/Activities/ActivityDemo2.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+public class ActivityDemo2 {
+	
+	@Test
+    public void TestCase() {
+        System.out.println("I'm in the test case from DemoTwo Class");
+    }
+
+}
