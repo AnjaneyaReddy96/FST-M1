503721b3c8976f52b38a8a266c6d12d5f0349ac6
AnjaneyaReddy96 <ajreddyedula@gmail.com>
2025-02-11 16:39:52 +0530

    TestRunner.java

--- Cucumber/Activities/TestRunner.java
+++ Cucumber/Activities/TestRunner.java
@@ -9,12 +9,12 @@ import io.cucumber.junit.platform.engine.Constants;
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("Features")
+@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "stepDefinitions")
+@ConfigurationParameter(key = Constants.FILTER_TAGS_PROPERTY_NAME, value = "@activity4")
 @ConfigurationParameter(
-key = Constants.GLUE_PROPERTY_NAME,
-value = "stepDefinitions")
-@ConfigurationParameter(
-key = Constants.FILTER_TAGS_PROPERTY_NAME,
-value = "@Activity3")
+	key = Constants.PLUGIN_PROPERTY_NAME,
+	value = "pretty, html:Reports/HTML_Report.html, junit:Reports/XML_Report.xml, json:Reports/JSON_Report.json"
+)
 
 public class TestRunner {
 
